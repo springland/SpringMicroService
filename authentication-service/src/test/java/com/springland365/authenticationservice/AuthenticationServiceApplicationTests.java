b7b78fe48c1aaa6ba70d27da9f46e6b5a648726b
fl <fenglinmail@yahoo.com>
2021-01-10 15:38:48 -0500

    Add test cases

---
+++ authentication-service/src/test/java/com/springland365/authenticationservice/AuthenticationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springland365.authenticationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ zuulsvc/src/test/java/com/springland365/zuulsvc/ZuulsvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springland365.zuulsvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulsvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
